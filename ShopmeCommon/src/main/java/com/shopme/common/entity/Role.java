ca09f8f816369ad3ef48286e19f16bbb7b55207f
Shubhgkr <shubhgkr@gmail.com>
2023-02-17 00:12:44 +0530

    Added User and UserRepository and related test methods

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -22,7 +22,6 @@ public class Role {
 	private String description;
 
 	public Role() {
-
 	}
 
 	public Role(Integer id) {
@@ -60,6 +59,36 @@ public class Role {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -0,0 +1,94 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testCreateNewUserWithOneRole() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		/*
+		 * NOTE: You can make a roleAdmin as well by this: Role roleAdmin=new Role(1);
+		 * but both are slight different. This one will create a new instance of the
+		 * 'Role' class and sets its property to 1. Here we will not access the
+		 * database.
+		 * 
+		 * while the entityManager will find an existing 'Role' with id of '1' from the
+		 * database. This is useful when the entity is already present in the database.
+		 * otherwise EntityManager will return 'null'
+		 * 
+		 */
+		User userNamHM = new User("nam@codejava.net", "nam2020", "Nam", "Ha Minh");
+		userNamHM.addRole(roleAdmin);
+		User savedUser = userRepository.save(userNamHM);
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateNewUserWithTwoRole() {
+		User userRavi = new User("ravi@gmail.com", "ravi2020", "Ravi", "Kumar");
+		Role roleEditor = new Role(3);
+		Role roleAssistant = new Role(5);
+		userRavi.addRole(roleEditor);
+		userRavi.addRole(roleAssistant);
+		User savedUser = userRepository.save(userRavi);
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = userRepository.findAll();
+		listUsers.forEach(user -> System.out.println(user));
+	}
+
+	@Test
+	public void testGetUserById() {
+		User userNam = userRepository.findById(1).get();
+		System.out.println(userNam);
+		assertThat(userNam).isNotNull();
+	}
+
+	@Test
+	public void testUpdateUserDetails() {
+		User userNam = userRepository.findById(1).get();
+		userNam.setEnabled(true);
+		userNam.setEmail("namjavaprogrammer@gmail.com");
+		userRepository.save(userNam);
+	}
+
+	@Test
+	public void testUpdateUserRoles() {
+		User userRavi = userRepository.findById(2).get();
+		Role roleEditor = new Role(3);
+		Role roleSalesperson = new Role(2);
+		
+		userRavi.getRoles().remove(roleEditor);
+		userRavi.addRole(roleSalesperson);
+		userRepository.save(userRavi);
+	}
+	
+	@Test
+	public void testDeleteUser() {
+		userRepository.deleteById(2);
+	}
+}
