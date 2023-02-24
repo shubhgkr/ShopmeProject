a3a37f79676053b23c3ad46ddf88cc2cb96dda3b
Shubhgkr <shubhgkr@gmail.com>
2023-02-25 01:29:55 +0530

    Code New User Function

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -63,7 +63,7 @@ public class Role {
 
 	@Override
 	public String toString() {
-		return "Role [name=" + name + "]";
+		return this.name;
 	}
 
 	@Override
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -5,15 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
 public class UserService {
 
 	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> listAll(){
+
+	public List<User> listAll() {
 		return (List<User>) userRepository.findAll();
+	}
+
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepository.findAll();
+	}
+
+	public void save(User user) {
+		userRepository.save(user);
 	}
 }
